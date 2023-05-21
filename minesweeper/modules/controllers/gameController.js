10e6576c2530a04191e0af1c811cb776174076d7
Rodion <rodionserez@gmail.com>
2023-05-21 18:38:42 +0300

    feat: change the logic of work, remove minefieldController module

--- minesweeper/modules/controllers/gameController.js
+++ minesweeper/modules/controllers/gameController.js
@@ -1,28 +1,45 @@
-import MinefieldController from './minefieldController';
 import createNode from '../helpers/createNode';
-import { getSettingsWidgetLayout, getInterfaceLayout } from '../helpers/getInterfaceLayout';
+import {
+ getSettingsWidgetLayout, getInterfaceLayout, getMinesCounterLayout, getMinefieldNode,
+} from '../helpers/getInterfaceLayout';
 import { defaultGameSettings, defaultColorTheme } from '../helpers/defaultGameSettings';
+import getMinefieldState from '../helpers/createMinefield';
+
+const fieldSizes = {
+	easy: {
+		sizeX: 10,
+		sizeY: 10,
+		minesAmount: 10,
+	},
+	medium: {
+		sizeX: 15,
+		sizeY: 15,
+		minesAmount: 55,
+	},
+	hard: {
+		sizeX: 25,
+		sizeY: 25,
+		minesAmount: 99,
+	},
+};
 
 export default class GameController {
 	constructor() {
-		window.addEventListener('beforeunload', () => this.setGameSettings()); // TODO
 		window.addEventListener('DOMContentLoaded', () => {
 			this.getGameSettings();
 			this.init();
 		});
+		window.addEventListener('unload', () => this.setGameSettings()); // TODO
 	}
 
 	init() {
-		/* create minefield */
-		if (!this.gameSettings.minefield) {
-			this.gameSettings.minefield = new MinefieldController(this.gameSettings);
-		}
 		this.setPageLayout();
 		this.setEventListeners();
 	}
 
 	reloadGame() {
-		this.gameSettings = { ...this.tempSettings };
+		this.gameSettings = structuredClone(this.tempSettings);
+		this.minefield = getMinefieldState(this.gameSettings);
 		this.setGameSettings();
 		document.querySelector('.wrapper').remove();
 		this.init();
@@ -30,27 +47,34 @@ export default class GameController {
 
 	/* get game settings from local storage and writes it to this.gameSettings */
 	getGameSettings() {
-		this.gameSettings = JSON.parse(localStorage.getItem('gameSettings'));
-		if (this.gameSettings === null) this.gameSettings = defaultGameSettings;
-		this.tempSettings = { ...this.gameSettings }; // object for writing temporary settings
-		this.colorTheme = JSON.parse(localStorage.getItem('colorTheme'));
-		if (this.colorTheme === null) this.colorTheme = defaultColorTheme;
+		if (!localStorage.getItem('gameSettings')) this.gameSettings = defaultGameSettings;
+		else this.gameSettings = JSON.parse(localStorage.getItem('gameSettings'));
+		this.tempSettings = structuredClone(this.gameSettings); // object for writing temporary settings
+
+		if (!localStorage.getItem('colorTheme')) this.colorTheme = defaultColorTheme;
+		else this.colorTheme = JSON.parse(localStorage.getItem('colorTheme'));
+
+		if (localStorage.getItem('minefield')) this.minefield = JSON.parse(localStorage.getItem('minefield'));
+		else this.minefield = getMinefieldState(this.gameSettings);
 	}
 
 	/* write game settings to local storage */
 	setGameSettings() {
-		localStorage.setItem('gameSettings', JSON.stringify(this.gameSettings));
-		localStorage.setItem('colorTheme', JSON.stringify(this.colorTheme));
+		if (this.gameSettings) localStorage.setItem('gameSettings', JSON.stringify(this.gameSettings));
+		if (this.minefield) localStorage.setItem('minefield', JSON.stringify(this.minefield));
+		if (this.colorTheme) localStorage.setItem('colorTheme', JSON.stringify(this.colorTheme));
 	}
 
 	setPageLayout() {
+		const settings = this.gameSettings;
 		const wrapper = createNode('div', `wrapper ${this.colorTheme}`);
 		const gameField = createNode('article', 'game-field');
 		const pageHeader = createNode('h1', 'game-name');
 		pageHeader.textContent = 'Minesweeper';
-		gameField.insertAdjacentHTML('afterbegin', getInterfaceLayout(this.gameSettings));
-		gameField.append(this.gameSettings.minefield.getMinefieldNode());
-		wrapper.insertAdjacentHTML('afterbegin', getSettingsWidgetLayout(this.gameSettings, this.colorTheme));
+		gameField.insertAdjacentHTML('afterbegin', getSettingsWidgetLayout(settings, this.colorTheme));
+		gameField.insertAdjacentHTML('beforeend', getInterfaceLayout(settings));
+		gameField.append(getMinefieldNode(this.minefield));
+		gameField.insertAdjacentHTML('beforeend', getMinesCounterLayout(settings));
 		wrapper.append(pageHeader, gameField);
 		document.body.prepend(wrapper);
 	}
