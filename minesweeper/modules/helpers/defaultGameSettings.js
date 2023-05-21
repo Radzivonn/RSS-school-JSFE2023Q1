b0addc20eabf267d800e113d0b25b5694a1677fb
Rodion <rodionserez@gmail.com>
2023-05-21 18:27:10 +0300

    feat: add new settings

--- minesweeper/modules/helpers/defaultGameSettings.js
+++ minesweeper/modules/helpers/defaultGameSettings.js
@@ -1,9 +1,14 @@
 export const defaultGameSettings = {
 	difficulty: 'easy',
 	minesAmount: 10,
-	gameTime: '0',
+	gameTime: 0,
 	gameState: 'in progress',
 	clicksAmount: 0,
+	fieldSize: {
+		sizeX: 10,
+		sizeY: 10,
+		minesAmount: 10,
+	},
 };
 
 export const defaultColorTheme = 'light';
