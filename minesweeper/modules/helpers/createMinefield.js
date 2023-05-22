c9d81b97b79a2f38c585bc5f08e5d8ffe133e4a1
Rodion <rodionserez@gmail.com>
2023-05-22 12:18:43 +0300

    feat: change data structure to objects instead of array

--- minesweeper/modules/helpers/createMinefield.js
+++ minesweeper/modules/helpers/createMinefield.js
@@ -1,13 +1,17 @@
 import getRandomInt from './getRandomNumber';
 
 const getEmptyMinefield = (gameSettings) => {
-	const minefield = [];
+	const minefield = {};
 	for (let y = 0; y < gameSettings.fieldSize.sizeY; y += 1) {
-		const minefieldRow = [];
+		const minefieldRow = {};
 		for (let x = 0; x < gameSettings.fieldSize.sizeX; x += 1) {
-			minefieldRow.push('');
+			minefieldRow[`${x}`] = {
+				isMined: false,
+				isOpened: false,
+				minesAmountNearby: 0,
+			};
 		}
-		minefield.push(minefieldRow);
+		minefield[`${y}`] = minefieldRow;
 	}
 	return minefield;
 };
@@ -18,8 +22,8 @@ const placeMines = (field, gameSettings) => {
 	for (let i = mines; i > 0;) {
 		const randY = getRandomInt(0, gameSettings.fieldSize.sizeY);
 		const randX = getRandomInt(0, gameSettings.fieldSize.sizeX);
-		if (resultField[randY][randX] === '') {
-			resultField[randY][randX] = 'bomb';
+		if (resultField[randY][randX].isMined === false) {
+			resultField[randY][randX].isMined = true;
 			i -= 1;
 		}
 	}
