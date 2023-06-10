adb241229f9f9262bd4d62537a9a14fec4d7f784
Rodion <rodionserez@gmail.com>
2023-06-10 15:18:02 +0300

    feat: migrate file to TS

---
+++ migration-to-ts/src/components/view/appView.ts
@@ -0,0 +1,27 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import { articlesData, newsData, sourceData, IView } from '../../types/index';
+
+export class AppView implements IView {
+    news: News;
+    sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: newsData): void {
+        console.log(data);
+        const values: articlesData = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: sourceData) {
+        console.log(data);
+        const values = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
