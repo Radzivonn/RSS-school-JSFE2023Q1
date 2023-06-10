87f1b5c454799d8b26e3e3f9cf4ad9367e863c14
Rodion <rodionserez@gmail.com>
2023-06-10 23:43:08 +0300

    feat: migrate file to TS

--- migration-to-ts/src/components/controller/loader.ts
+++ migration-to-ts/src/components/controller/loader.ts
@@ -1,11 +1,15 @@
-class Loader {
-    constructor(baseLink, options) {
+import { sourceData, ILoader, loaderOptions, respOptions, requestOptions, urlOptions } from '../../types/index';
+
+class Loader implements ILoader {
+    baseLink: string;
+    options: loaderOptions;
+    constructor(baseLink: string, options: loaderOptions) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: requestOptions,
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,28 +17,27 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    private errorHandler(res: Response) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
             throw Error(res.statusText);
         }
-
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    private makeUrl(options: respOptions, endpoint: string) {
+        const urlOptions: urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
-        Object.keys(urlOptions).forEach((key) => {
+        Object.keys(urlOptions).forEach((key: string) => {
             url += `${key}=${urlOptions[key]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: string, endpoint: string, callback: (data?: sourceData) => void, options: respOptions = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
