a39e7f7bdfff78432f806165a92f88ba55ab1d63
Rodion <rodionserez@gmail.com>
2023-06-12 20:31:18 +0300

    build: add rule for images

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -10,6 +10,10 @@ const baseConfig = {
     module: {
         rules: [
             {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
