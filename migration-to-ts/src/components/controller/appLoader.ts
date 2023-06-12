9770a7c2b9ec61597ecefcdb68539fdec03745b9
Rodion <rodionserez@gmail.com>
2023-06-12 20:35:18 +0300

    feat: change URL

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '7ca0f75d7e094795b7e58c3ec8946014', // получите свой ключ https://newsapi.org/
         });
     }
