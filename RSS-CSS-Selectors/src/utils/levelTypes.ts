2684bc22614a89c2f0d4412393b4056cbb8dd7d9
Rodion <rodionserez@gmail.com>
2023-06-29 18:38:13 +0300

    feat: add types for level objects

---
+++ RSS-CSS-Selectors/src/utils/levelTypes.ts
@@ -0,0 +1,20 @@
+type LevelDescription = {
+	levelTitle: string,
+	brief: string,
+	syntax: string,
+	taskDescription: string,
+	example: string,
+}
+
+export type Level = Readonly<{
+	levelNumber: number,
+	markup: string,
+	html: string,
+	correctSelectors: Array<string>,
+	task: string,
+	description: LevelDescription,
+}>
+
+export type completedLevels = Array<number>;
+
+export type LevelsList = Readonly<Array<Level>>;
