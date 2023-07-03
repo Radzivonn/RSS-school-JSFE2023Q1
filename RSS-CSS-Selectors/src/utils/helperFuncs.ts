14c5a280ad97c6c25b72652815fde3004d5d942c
Rodion <rodionserez@gmail.com>
2023-07-03 16:07:34 +0300

    feat: add funcs for sync illumination of blocks

--- RSS-CSS-Selectors/src/utils/helperFuncs.ts
+++ RSS-CSS-Selectors/src/utils/helperFuncs.ts
@@ -16,3 +16,16 @@ export const createElement = (params: Params): HTMLElement => {
 export const removeClasses = (elements: HTMLCollection, className: string): void => {
 	for (const child of elements) child.classList.remove(className);
 }
+
+export const getEventElement = (e: Event, childClassName: string): HTMLElement | null => {
+	return (<HTMLElement>e.target).closest(`.${childClassName}`);
+}
+
+export const hoverHandler = (e: Event, childClassName: string): void => {
+	const hoveredElement = getEventElement(e, childClassName);
+	if (hoveredElement) {
+		document.querySelectorAll(`[number="${hoveredElement.getAttribute('number')}"]`).forEach(element => {
+			element.classList.toggle('hovered');
+		});
+	}
+}
