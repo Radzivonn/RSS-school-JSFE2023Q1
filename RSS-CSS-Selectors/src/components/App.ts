51fde6d82e98f42bf26559e81fc004d3e20df99e
Rodion <rodionserez@gmail.com>
2023-07-04 00:02:00 +0300

    feat: add CSSEditor component to App

--- RSS-CSS-Selectors/src/components/App.ts
+++ RSS-CSS-Selectors/src/components/App.ts
@@ -1,21 +1,25 @@
 import LevelsBlockController from "@/components/LevelsBlock/LevelsBlockController";
 import LayoutExampleController from '@/components/LayoutExample/LayoutExampleController';
 import HTMLViewerController from "@/components/HTMLViewer/HTMLViewerController";
+import CSSEditorController from "@/components/CSSEditor/CSSEditorController";
 
 export default class App {
 	private levelsBlock: LevelsBlockController;
 	private layoutExample: LayoutExampleController;
 	private htmlViewer: HTMLViewerController;
+	private cssEditor: CSSEditorController;
 	
 	constructor() {
 		this.levelsBlock = new LevelsBlockController();
 		this.layoutExample = new LayoutExampleController();
 		this.htmlViewer = new HTMLViewerController();
+		this.cssEditor = new CSSEditorController();
 	}
 
 	public init(): void {
 		this.layoutExample.init();
 		this.htmlViewer.init();
+		this.cssEditor.init();
 		this.levelsBlock.init();
 	}
 }
