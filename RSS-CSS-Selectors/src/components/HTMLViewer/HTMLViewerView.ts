555edc38729c7bba22fc2077682f03fb7dd67490
Rodion <rodionserez@gmail.com>
2023-07-05 17:35:36 +0300

    feat: implement code highlight

--- RSS-CSS-Selectors/src/components/HTMLViewer/HTMLViewerView.ts
+++ RSS-CSS-Selectors/src/components/HTMLViewer/HTMLViewerView.ts
@@ -1,18 +1,22 @@
 import { IView } from "./types";
 import { ConfigElement } from "@/utils/levels/levelTypes";
 import { createElement } from "@/utils/helperFuncs";
+import hljs from "highlight.js/lib/common";
+import '@/styles/monokai-sublime.min.css';
 
 export default class HTMLViewerView implements IView {
-	root: HTMLElement;
+	public root: HTMLElement;
+	private hljs = hljs;
 	constructor(root: HTMLElement) {
 		this.root = root;
+		this.hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
 	}
 
 	public updateView(elements: Array<ConfigElement>): void {
 		this.root.replaceChildren(
-			createElement({	tag: 'pre', text: `<div class="table">\n` }),
+			...this.highLightElements(createElement({	tag: 'pre', text: `<div class="table">\n` })),
 			...this.getMarkupElements(elements),
-			createElement({	tag: 'pre', text: `</div>\n` }),
+			...this.highLightElements(createElement({	tag: 'pre', text: `</div>\n` })),
 		);
 	}
 
@@ -26,15 +30,32 @@ export default class HTMLViewerView implements IView {
 	/* recursion function */
 	private createMarkupElement(element: ConfigElement, uniqueNumber: number): HTMLElement {
 		const node = createElement({ tag: 'div', classNames: ['html__markup-code'] });
-		if(typeof uniqueNumber === 'number') node.setAttribute('number', `${uniqueNumber}`);
-		node.append(createElement({
+		if (typeof uniqueNumber === 'number') node.setAttribute('number', `${uniqueNumber}`);
+		const highlightingElements = [this.createOpenTag(element)]
+		if (element.child) {
+			const childElement = this.createMarkupElement(element.child, uniqueNumber + 10);
+			highlightingElements.push(childElement, this.createCloseTag(element));
+		}
+		node.append(...this.highLightElements(...highlightingElements));
+		return node;
+	}
+
+	private createOpenTag(element: ConfigElement): HTMLElement {
+		return createElement({
 			tag: 'pre',
 			text: `<${element.tag}${element.className ? ` class=${element.className}` : ''}${element.id ? ` id=${element.id}` : ''}${element.child ? '' : '/'}>\n`
-		}));
-		if (element.child) {
-			node.append(this.createMarkupElement(element.child, uniqueNumber + 10));
-			node.append(createElement({ tag: 'pre', text: `</${element.tag}>\n` }));
-		}
-		return node;
+		});
+	}
+
+	private createCloseTag(element: ConfigElement): HTMLElement {
+		return createElement({
+			tag: 'pre',
+			text: `</${element.tag}>\n`
+		});
+	}
+
+	private highLightElements(...elements: HTMLElement[]): HTMLElement[] {
+		elements.forEach(element => this.hljs.highlightElement(element));
+		return elements;
 	}
 }
