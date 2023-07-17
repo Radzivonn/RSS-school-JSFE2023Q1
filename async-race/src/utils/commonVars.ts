ce48285c8a60ea7e08d952d041953bccc524aee0
Rodion <rodionserez@gmail.com>
2023-07-17 14:23:40 +0300

    refactor: change names

--- async-race/src/utils/commonVars.ts
+++ async-race/src/utils/commonVars.ts
@@ -1,7 +1,7 @@
 export const BASEREQUESTtURL = 'http://127.0.0.1:3000';
 
 export enum RequestDirs {
-	ALLCARSDATA = 'garage',
-	ALLWINNERSDATA = 'winners',
-	ENGINE = 'engine',
+	ALLCARSPATH = 'garage',
+	ALLWINNERSPATH = 'winners',
+	ENGINEPATH = 'engine',
 }
