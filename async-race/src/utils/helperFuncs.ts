14b178499ded7ffd16906aa03376a4504c632145
Rodion <rodionserez@gmail.com>
2023-07-17 12:16:33 +0300

    feat: add helper functions

--- async-race/src/utils/helperFuncs.ts
+++ async-race/src/utils/helperFuncs.ts
@@ -1,3 +1,6 @@
+import { BASEREQUESTtURL } from './commonVars';
+import carSVGcode from './carSVGcode';
+
 type ElementParams = Readonly<{
 	tag: string,
 	classNames?: string[],
@@ -16,4 +19,28 @@ export const createElement = (params: ElementParams): HTMLElement => {
 	element.textContent = params.text ? params.text : '';
 	if (params.attr) element.setAttribute(params.attr.attrName, params.attr.attrValue);
 	return element;
+};
+
+export const createPaginationButtons = (): HTMLElement[] =>
+	[
+		createElement({ tag: 'button', classNames: ['button', 'previous-button'], text: 'prev' }),
+		createElement({ tag: 'button', classNames: ['button', 'next-button'], text: 'next' }),
+	];
+
+export const getRequestData = async <T>(URL: string): Promise<T> => {
+	const data = await fetch(URL);
+	if (data.ok) return data.json();
+	throw new Error(data.statusText);
+};
+
+export const getCarsData = async <T>(requestDir: string): Promise<T> => {
+	const data = await getRequestData<T>(`${BASEREQUESTtURL}/${requestDir}`);
+	return data;
+};
+
+export const createCarImg = (color: string): HTMLElement => {
+	const car = createElement({ tag: 'div', classNames: ['car'] });
+	car.style.fill = color;
+	car.insertAdjacentHTML('afterbegin', carSVGcode);
+	return car;
 };
