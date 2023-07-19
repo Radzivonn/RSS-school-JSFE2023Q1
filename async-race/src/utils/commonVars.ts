191ca08d3d9fd4c51d8f97fc703dbf7a2dbb94d6
Rodion <rodionserez@gmail.com>
2023-07-19 10:01:36 +0300

    feat: add car generation data

--- async-race/src/utils/commonVars.ts
+++ async-race/src/utils/commonVars.ts
@@ -5,3 +5,9 @@ export enum RequestDirs {
 	WINNERSDATAPATH = 'winners',
 	ENGINEDATAPATH = 'engine',
 }
+
+export const carNames = ['Tesla', 'Ford', 'BMW', 'Mersedes-Benz', 'Porshe', 'Lada', 'lamborghini', 'Chevrolet', 'Nissan', 'Mitsubishi', 'Audi'];
+
+export const carModels = ['Model X', 'Mustang', 'X5', 'Maybach', 'Carrera', 'Priora', 'Gallardo', 'Camaro', 'GTR', 'Lancer Evolution X', 'R8'];
+
+export const carColors = ['black', 'red', 'yellow', 'aquamarine', 'cyan', 'white', 'purple', 'orange', 'gold', 'blue'];
