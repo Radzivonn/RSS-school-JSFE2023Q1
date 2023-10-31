355b608ca517e0778b8d9abb9a9867fdd4a6652f
Rodion <rodionserez@gmail.com>
2023-10-31 15:26:21 +0300

    feat: add constants folder

---
+++ minesweeper/constants/sounds.js
@@ -0,0 +1,3 @@
+export const clickSoundURL = 'https://bigsoundbank.com/UPLOAD/mp3/1742.mp3';
+export const winSoundURL = 'https://bigsoundbank.com/UPLOAD/mp3/0237.mp3';
+export const loseSoundURL = 'https://bigsoundbank.com/UPLOAD/mp3/1023.mp3';
